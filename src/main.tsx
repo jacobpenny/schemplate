434d958a6d7c67fe703348d2662bc31d5c328fdf
Jacob Penny <jacob.penny@gmail.com>
2023-09-14 19:25:25 -0300

    Add prettier, css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import "./reset.scss";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
